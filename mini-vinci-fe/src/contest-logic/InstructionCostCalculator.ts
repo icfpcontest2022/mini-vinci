bc0a47892d335379ac628517f0fb7401743a2a96
Alperen Keles <alpkeles99@gmail.com>
2022-09-02 03:10:04 -0700

    change cost logic

--- mini-vinci-fe/src/contest-logic/InstructionCostCalculator.ts
+++ mini-vinci-fe/src/contest-logic/InstructionCostCalculator.ts
@@ -7,17 +7,17 @@ export class InstructionCostCalculator {
     static baseCostMap: Map<InstructionType, number> = new Map([
         [InstructionType.NopInstructionType,            0],
         [InstructionType.CommentInstructionType,        0],
-        [InstructionType.ColorInstructionType,          3],
-        [InstructionType.VerticalCutInstructionType,    4],
-        [InstructionType.HorizontalCutInstructionType,  4],
-        [InstructionType.PointCutInstructionType,       5],
-        [InstructionType.SwapInstructionType,           2],
+        [InstructionType.ColorInstructionType,          5],
+        [InstructionType.VerticalCutInstructionType,    7],
+        [InstructionType.HorizontalCutInstructionType,  7],
+        [InstructionType.PointCutInstructionType,       10],
+        [InstructionType.SwapInstructionType,           3],
         [InstructionType.MergeInstructionType,          1],
     ])
 
     static getCost(instructionType: InstructionType, blockSize: number, canvasSize: number): number {
         const baseCost = this.baseCostMap.get(instructionType) as number;
-        const totalCost = Math.round(baseCost * Math.sqrt(canvasSize / blockSize));
+        const totalCost = Math.round(baseCost * (canvasSize / blockSize));
         return totalCost;
     }
 }
--- mini-vinci-fe/src/contest-logic/SimilarityChecker.ts
+++ mini-vinci-fe/src/contest-logic/SimilarityChecker.ts
@@ -10,24 +10,6 @@ type HSV = {
 }
 
 export class SimilarityChecker {
-
-  static pngToFrame(filename: string): Frame {
-    // let frame: Frame = [];
-    // var fs = require("fs"),
-    // PNG = require("pngjs").PNG;
-
-    // var data = fs.readFileSync(filename);
-    // var png = PNG.sync.read(data);
-    // for (var y = 0; y < png.height; y++) {
-    //   for (var x = 0; x < png.width; x++) {
-    //     var idx = (png.width * y + x) << 2;
-    //     frame.push(new RGBA(png.data.slice(idx, idx + 4)));
-    //   }
-    // }
-    // return frame;
-    return []
-  }
-
   static dataToFrame(data: [number, number, number, number][]): Frame {
     let frame: Frame = [];
 
@@ -39,63 +21,22 @@ export class SimilarityChecker {
   }
 
   static imageDiff(f1: Frame, f2: Frame): number {
-      let diff = 0;
-      for (let index = 0; index < f1.length; index++) {
-          const p1 = f1[index];
-          const p2 = f2[index];
-          diff += this.pixelDiff(p1, p2);
-      }
-      return Math.round(diff);
+    let diff = 0;
+    let alpha = 0.005;
+    for (let index = 0; index < f1.length; index++) {
+        const p1 = f1[index];
+        const p2 = f2[index];
+        diff += this.pixelDiff(p1, p2);
+    }
+    return Math.round(diff * alpha);
   }
 
   static pixelDiff(p1: RGBA, p2: RGBA): number {
-      const p1AsHsv = this.rgbToHsv(p1);
-      const p2AsHsv = this.rgbToHsv(p2);
-      const hueDist = Math.min(Math.abs(p1AsHsv.hue - p2AsHsv.hue), 360 - Math.abs(p1AsHsv.hue - p2AsHsv.hue)) / 180.0;
-      const satDist = Math.abs(p1AsHsv.sat - p2AsHsv.sat);
-      const valDist = Math.abs(p1AsHsv.val - p2AsHsv.val) / 255.0;
-      const distance = Math.sqrt(hueDist * hueDist + satDist * satDist + valDist * valDist);
-      return distance;
+    const rDist = (p1.r - p2.r) * (p1.r - p2.r);
+    const gDist = (p1.g - p2.g) * (p1.g - p2.g);
+    const bDist = (p1.b - p2.b) * (p1.b - p2.b);
+    const aDist = (p1.a - p2.a) * (p1.a - p2.a);
+    const distance = Math.sqrt(rDist + gDist + bDist + aDist);
+    return distance;
   }
-
-  static rgbToHsv(rgba: RGBA): HSV {
-      const r = rgba.r / 255;
-      const g = rgba.g / 255;
-      const b = rgba.b / 255;
-      const maxc = Math.max(r, g, b);
-      const minc = Math.min(r, g, b);
-      const v = maxc;
-      if (minc == maxc) {
-          return { hue: 0.0, sat: 0.0, val: v };
-      }
-
-      const s = ((maxc-minc) / maxc) * 100;
-      const rc = (maxc-r) / (maxc-minc);
-      const gc = (maxc-g) / (maxc-minc);
-      const bc = (maxc-b) / (maxc-minc);
-      let h: number;
-      switch (maxc) {
-          case r: {
-              h = 0.0 + bc - gc;
-              break;
-          }
-          case g: {
-              h = 2.0 + rc - bc;
-              break;
-          }
-          case b: {
-              h = 4.0 + gc - rc;
-              break;
-          }
-          default:
-              return { hue: 0.0, sat: 0.0, val: v };
-      }
-
-      const hue = ((h/6.0) % 1.0) * 360;
-      const val = v * 100;
-      const sat = s * 100;
-
-      return { hue, sat, val };
-  }
-
 }
